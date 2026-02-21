85fee9c4e0e1e1ffe1c86d7d193d6d9cfeee81b2
Hafsa Imran <hafsaemran1712@gmail.com>
2026-02-21 13:01:39 +0500

    push

---
+++ app.js
@@ -0,0 +1,11 @@
+question:1
+// alert("hello")
+question:2
+// alert("wellcome to our website")
+question:3
+// alert("wellcome to JS land \n Happy coding!");
+question:4
+// alert("wellcome to JS land...");
+// alert("Happy coding!");
+question:5
+// alert("hello...I can run JS through my web browser's console");
