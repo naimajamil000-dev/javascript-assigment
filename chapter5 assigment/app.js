dc8589fd6e3735905593ace633cefe59293c6d1a
maimoona1234-lab <fhina2510@gmail.com>
2026-03-07 13:42:37 +0500

    push

---
+++ chapter5 assigment/app.js
@@ -0,0 +1,27 @@
+// question 1
+var num1 =3;
+var num2 =5;
+
+var sum = num1+num2;
+document.write("sum of "+num1+"and"+ num2 + "is" + sum);
+document.write("<br/>")
+// question 2
+var1 = 10;
+var2= 5;
+document.write("subtraction:"+(1-2)+"<br>");
+document.write("multiplication:"+(1*2)+"<br>");
+document.write("devision:"+(1/2)+"<br>");
+document.write("modulus:"+(1%2)+"<br>");
+// question 3
+var num;
+document.write("value after variable declaration is:"+num+"<br>");
+num=7;
+document.write("initial value :" + num + "<br>");
+num++
+document.write("value after increment is:"+ num + "<br>")
+num=num+7;
+document.write("value after addition is : "+ num+"<br>")
+num--;
+document.write("value after decrement is :"+ num + "<br>");
+var reminder = num % 3;
+document.write("the remainder is :"+reminder);
