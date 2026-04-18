2d9c4a852c6ffafe07030ae57c273f9c9cd460bc
naimajamil <naimajamil000@gmail.com>
2026-04-18 15:46:01 +0500

    push

---
+++ chapter 6to9/app.js
@@ -0,0 +1,175 @@
+// Q #1
+
+// let value = 10
+// document.write(`Result: <br/> The value of a is: ${value}`)
+// document.write(`<br/>`)
+
+// document.write(`<br/>`)
+// document.write(`The value of ++a is: 11`)
+// document.write(`<br/>`)
+// ++value
+// document.write(`Now the value of a is: ${value}`)
+// document.write(`<br/>`)
+
+// document.write(`<br/>`)
+// document.write(`The value of a++ is: 11`)
+// document.write(`<br/>`)
+// value++
+// document.write(`Now the value of a is: ${value}`)
+// document.write(`<br/>`)
+
+// document.write(`<br/>`)
+// document.write(`The value of --a is: 11`)
+// document.write(`<br/>`)
+// --value
+// document.write(`Now the value of a is: ${value}`)
+// document.write(`<br/>`)
+
+// document.write(`<br/>`)
+// document.write(`The value of a-- is ${value}`)
+// document.write(`<br/>`)
+// value--
+// document.write(`Now the value of a is: ${value}`)
+
+// Q #2
+
+// let a = 2
+// let b = 1
+// document.write(`a is ${a}`)
+// document.write(`<br/>`)
+
+// document.write(`b is ${b}`)
+// document.write(`<br/>`)
+
+// let result = --a - --b + ++b + b--
+// document.write(`result is ${result}`)
+// document.write(`<br/>`)
+
+// Q #3
+
+// let userName = prompt("User Name")
+
+// if(userName = "name"){
+// alert("Welcome to the Page")
+// }
+
+// Q #4
+
+// let num = +prompt("any number");
+
+// if (num >= 0) {
+//   let multiple = 1;
+//   document.write(`Table of ${num}`);
+//   document.write("<br />");
+
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+// } else {
+//   document.write(`Table of 5`);
+//   document.write("<br />");
+//   let table = 5;
+//   let multiple = 1;
+
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+// }
+
+// Q #5
+
+// let subject1 = prompt("1st Subject")
+// let subject1Marks = +prompt("1st Subject Marks")
+
+// let subject2 = prompt("2nd Subject")
+// let subject2Marks = +prompt("2nd Subject Marks")
+
+// let subject3 = prompt("3rd Subject")
+// let subject3Marks = +prompt("3rd Subject Marks")
+
+// let totalMarks = 100
+
+// document.write(`Subject Total Marks Obtained Mraks Percentage`)
+// document.write("<br />")
+// document.write(`${subject1} ${totalMarks} ${subject1Marks} ${subject1Marks}%`)
+
+// document.write("<br />")
+// document.write(`${subject2} ${totalMarks} ${subject2Marks} ${subject2Marks}%`)
+
+// document.write("<br />")
+// document.write(`${subject3} ${totalMarks} ${subject3Marks} ${subject3Marks}%`)
+
+// let marks = totalMarks * 3
+// let obtainMarks = subject1Marks + subject2Marks + subject3Marks
+// let percentage = obtainMarks / marks * 100
+
+// document.write("<br />")
+// document.write(`${marks} ${obtainMarks} ${percentage}`)
