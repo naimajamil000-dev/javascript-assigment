4aba3e82ddef1401ce8beb33f9da3cede2f3518a
manaljawaid9 <manaljawaid9@gmail.com>
2026-04-20 01:10:31 +0500

    done

---
+++ chapter 2/app.js
@@ -0,0 +1,62 @@
+// question 1
+
+var userName = "ayesha";
+console.log(userName);
+
+// question 2
+
+var myName = "Naimajamil";
+console.log(myName);
+
+// question 3
+
+// a) Declare a JS variable titled message
+var message;
+
+// b) Assign "Hello World" to variable message
+message = "Hello World";
+
+// c) Display the message in alert box
+alert(message);
+
+// question 4
+
+var studentName = "Jhon Doe";
+var studentAge = "15 years old";
+var studentCourse = "certified mobile application development";
+
+alert (studentName);
+alert (studentAge);
+alert (studentCourse);
+
+// quetion 5
+
+var pizzaPattern = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+alert (pizzaPattern);
+
+// question 6
+
+var email = "naimajamil000@gmail.com";
+
+alert ("my email address is " + email);
+
+// question 7
+
+var book = "A Smarter Way To Learn Javascript";
+
+alert ("I am turn to learn from the Book" + book);
+
+// question 8
+
+var browser = "Yah! I can write HTML content through JavaScript";
+
+document.write(browser);
+
+// question 9
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+let(design);
+
+document.write(design);
