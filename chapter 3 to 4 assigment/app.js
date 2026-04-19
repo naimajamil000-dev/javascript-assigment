ff8c82aa19cc6a570278bb85cc88207f1070b9b1
manaljawaid9 <manaljawaid9@gmail.com>
2026-04-20 01:25:23 +0500

    done

---
+++ chapter 3 to 4 assigment/app.js
@@ -0,0 +1,60 @@
+// CHAPTER# 3
+
+// question 1
+
+// var age = "I am 18 years old";
+
+// alert (age);
+
+// question 2
+
+// var visitCount = "14";
+
+// alert ("You have to visit this site " + visitCount +" Times");
+
+// question 3
+
+// var birthYear = " 1990";
+
+// document.write ("My birth year is" + birthYear + "<br>");
+// document.write ("Data type of my Declared variable is number");
+
+// question 4
+
+// var visiterName = "John Doe";
+// var productsTitle = "T-shirt(s)";
+// var quantity = "5";
+
+// document.write (visiterName + " ordered " + quantity + productsTitle + " on XYZ Clothing store");
+
+// CHAPTER 4
+
+// question 1
+
+// var firstName, lastName, age;
+
+// question 2
+
+// legal variable name 
+
+//  myVariable, $price, _user_name, userName123, TOTAL_AMOUNT
+
+// illegal variable name
+
+//  1stVariable, my variable, user-name, var, @email
+
+//  question 3
+// a.heading
+document.write ("<h1> Rules for naming JS variables</h1>");
+
+// b. Content rules
+document.write ("<p>Variable names can only contain <b>numbers</b>, <b>$</b>, and <b>_</b>. For example: $my_1stVariable</p>");
+
+// c. Starting rules
+document.write ("<p>Variables must begin with a <b>letter</b>, <b>$</b> or <b>_</b>. For example: $name, _name or name</p>");
+
+// d. Case sensitivity
+document.write ("<p>Variable names are case <b>sensitive</b></p>");
+
+// e. Reserved words
+document.write ("<p>Variable names should not be JS <b>keywords</b></p>");
